4216f4e960f4114ba6450989480b53bcfc8db1f7
Keval Morabia <kmorabia@gmail.com>
2017-11-14 13:30:13 +0530

    input and output files created

--- src/TypeCheck.java
+++ src/TypeCheck.java
@@ -1,9 +1,16 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class TypeCheck {
 
-	public static void main(String[] args) {
-		
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("Data/Input.txt"));
+		String s;
+		while((s = br.readLine())!=null) {
+			System.out.println(s);
+		}
+		br.close();
 	}
 
 }
